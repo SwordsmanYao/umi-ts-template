ece506a32f25355d5d36bebaa1d0b08f6d1c415f
SwordmanYao <swordsman.yao@foxmail.com>
2019-04-25 15:52:04 +0800

    add react-use

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -9,17 +9,21 @@
 import React, { useEffect } from 'react';
 import styles from './index.less';
 import useListContext from './contexts/useListContext';
+import { useAsyncRetry } from 'react-use';
 
 export default () => {
   const { list, fetchList } = useListContext();
+  const { loading, retry } = useAsyncRetry(fetchList);
   useEffect(() => {
     fetchList();
   }, []);
   return (
     <div className={styles.normal}>
+      {loading && 'loading...'}
       {list.map(x => (
         <div key={x.id}>{x.name}</div>
       ))}
+      <button onClick={retry}>retry</button>
     </div>
   );
 };
